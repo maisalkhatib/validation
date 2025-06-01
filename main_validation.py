75d31a6197ca27efb6f4447424bbc00040bcaf6d
uzair-qltyss <35056054+uzair-b-m@users.noreply.github.com>
2025-06-01 08:45:02 +0300

    added request_id following UUIDv4 to the recieved request structure

--- main_validation.py
+++ main_validation.py
@@ -1,4 +1,20 @@
+from typing import Literal, Optional
+from pydantic import BaseModel
+
+from inventory_manager import InventoryManager
+
+
+
+
 class MainValidation:
     def __init__(self):
+
+
+        # the inventory manager
+        self._inventory = InventoryManager()
+        # the main queue where we will receive the requests
         self._queue = []
 
+    def post_request(self, request):
+        # check if it is a valid request using pydantic
+        pass
